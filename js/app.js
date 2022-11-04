4cde4137e602694dda53ebd698b3e8419329e385
OnlyDropped <asheburenkov@yandex.ru>
2022-11-04 19:16:40 +0300

    create certificates.html

--- js/app.js
+++ js/app.js
@@ -3,6 +3,30 @@ $('[data-fancybox]').fancybox({
   animationEffect   : 'slide-in-out'
 });
 
+
+
+// Fancybox Configuration
+$('[data-fancybox="gallery"]').fancybox({
+  buttons: [
+    "slideShow",
+    "thumbs",
+    "zoom",
+    "fullScreen",
+    "share",
+    "close"
+  ],
+  loop: true,
+  arrows: true,
+  toolbar: "false",
+  animationDuration: 900,
+  animationEffect: 'fade',
+  protect: true,
+  transitionEffect: "zoom-in-out",
+  transitionDuration: 1000,
+  clickSlide: "false",
+  animationEffect: "zoom-in-out"
+});
+
 // ymaps.ready(init);
 // function init(){
 //     var myMap = new ymaps.Map("map", {
