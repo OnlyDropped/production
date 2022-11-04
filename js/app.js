70942a968355a2dc97d9fcfb910ab6f8e5eea00b
OnlyDropped <asheburenkov@yandex.ru>
2022-11-04 16:49:59 +0300

    create and style contact.html

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ $('[data-fancybox]').fancybox({
   animationDuration : 900,
   animationEffect   : 'slide-in-out'
 });
+
+// ymaps.ready(init);
+// function init(){
+//     var myMap = new ymaps.Map("map", {
+//         center: [55.76, 37.64],
+//         zoom: 5
+//     });
+// }
